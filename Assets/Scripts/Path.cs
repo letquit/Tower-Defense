75359a47ed60696d0669194d2f894ac7163df2c5
letquit <1446444589@qq.com>
2026-03-04 15:17:56 +0800

    Following a path

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,11 +1,18 @@
 using System;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 public class Path : MonoBehaviour
 {
     public GameObject[] Waypoints;
 
+    public Vector3 GetPosition(int index)
+    {
+        return Waypoints[index].transform.position;
+    }
+    
     private void OnDrawGizmos()
     {
         if (Waypoints.Length > 0)
@@ -15,8 +22,9 @@ public class Path : MonoBehaviour
                 GUIStyle style = new GUIStyle();
                 style.normal.textColor = Color.white;
                 style.alignment = TextAnchor.MiddleCenter;
+#if UNITY_EDITOR
                 Handles.Label(Waypoints[i].transform.position + Vector3.up * 0.7f, Waypoints[i].name, style);
-                
+#endif
                 if (i < Waypoints.Length - 1)
                 {
                     Gizmos.color = Color.gray;
